135cd2f42aeea2f809e9c6c1c05b0739b2b498ef
Albert Gomes <albert.gomes@liferay.com>
2024-04-20 23:30:21 -0300

    Improviments in class print util

---
+++ src/main/java/com/liferay/convert/tools/util/PrintLoggerUtil.java
@@ -0,0 +1,71 @@
+package com.liferay.convert.tools.util;
+
+import java.time.Instant;
+import java.util.regex.Pattern;
+
+/**
+ * @author Albett Gomes Cabral
+ */
+public class PrintLoggerUtil {
+
+    public static void printInfo(String word) {
+        _print(_GREEN, word);
+    }
+
+    public static void printError(String word, String cause) {
+        if (cause == null) {
+            _print(_RED, word);
+        }
+        else {
+            _print(_RED, word + _BREAK_LINE +  cause);
+        }
+    }
+
+    public static void printReplacement(
+            String oldContent, String newContent, Pattern pattern) {
+
+        System.out.println("Apply pattern " + pattern.pattern());
+
+        System.out.println(
+                "Replaced " + _LIGHT_BLUE + oldContent + _RESET +
+                        _BREAK_LINE + " by " + _GREEN + newContent +
+                            _RESET + _DOUBLE_BREAK_LINE);
+
+    }
+
+    public static void printWarning(String word, String cause) {
+        if (cause == null) {
+            _print(_YELLOW, word);
+        }
+        else {
+            _print(_YELLOW, word + _BREAK_LINE +  cause);
+        }
+    }
+
+    private static void _print(String color, String word) {
+        System.out.println(
+                Instant.now() + _STRING_WHITE_SPACE + color
+                        + _STRING_WHITE_SPACE + word + _RESET);
+    }
+
+    // utilities variables
+
+    private static final String _DOUBLE_BREAK_LINE = "\n\n";
+
+    private static final String _BREAK_LINE = "\n";
+
+    private static final String _STRING_WHITE_SPACE = " ";
+
+    // colors variables
+
+    private static final String _GREEN = "\u001B[32m";
+
+    private static final String _LIGHT_BLUE = "\u001B[94m";
+
+    private static final String _RED = "\u001B[31m";
+
+    private static final String _RESET = "\u001B[0m";
+
+    private static final String _YELLOW = "\u001B[33m";
+
+}
