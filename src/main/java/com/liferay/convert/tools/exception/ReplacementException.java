b11819552f00350a0204352ac51c45de119d8aea
Albert Gomes <albert.gomes@liferay.com>
2024-04-20 23:29:41 -0300

    Create exceptions classes

---
+++ src/main/java/com/liferay/convert/tools/exception/ReplacementException.java
@@ -0,0 +1,16 @@
+package com.liferay.convert.tools.exception;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+public class ReplacementException extends Exception {
+
+    public ReplacementException(String message) {
+        super(message);
+    }
+
+    public ReplacementException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ src/main/java/com/liferay/convert/tools/exception/SQLFilesException.java
@@ -0,0 +1,16 @@
+package com.liferay.convert.tools.exception;
+
+/**
+ * @author Albert Gomes Cabral
+ */
+public class SQLFilesException extends Exception {
+
+    public SQLFilesException(String message) {
+        super(message);
+    }
+
+    public SQLFilesException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
